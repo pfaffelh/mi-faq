50a5e7a836027552360c5979a9973867e1e64a2c
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-04-09 13:17:22 +0200

    changed ip

--- misc/config.py
+++ misc/config.py
@@ -1,9 +1,14 @@
+import socket
+
 # Das ist der LDAP-Server der Universität, der dür die Authentifizierung verwendet wird.
 server="ldaps://ldap.uni-freiburg.de"
 base_dn = "ou=people,dc=uni-freiburg,dc=de"
 
-# Hier ist die MongoDB
-mongo_location = "mongodb://127.0.0.1:27017"
+# Hier ist die MongoDBsocket.gethostname()
+if socket.gethostname() == "www2":
+    mongo_location = "mongodb://localhost:27017"
+else:
+    mongo_location = "mongodb://127.0.0.1:27017"
 
 # Name der Berechtigung für diese App in der Datenbank
 app_name = "faq"
