f9a3ac7398e448ef644edb22af41a4e813c82ecd
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-08-03 22:11:21 +0200

    changed typo

--- pages/02_stu_qa.py
+++ pages/02_stu_qa.py
@@ -35,14 +35,14 @@ if st.session_state.logged_in:
 
     cats = list(util.stu_category.find(sort=[("rang", pymongo.ASCENDING)]))
 
-    cat = st.selectbox(label="Kategorie", options = [x["_id"] for x in cats], index = None, format_func = (lambda id : tools.repr(util.stu_category, id, False)), placeholder = "Wähle eine Kategorie", label_visibility = "collapsed")
+    cat = st.selectbox(label="Kategorie", options = [x['_id'] for x in cats], index = None, format_func = (lambda id : tools.repr(util.stu_category, id, False)), placeholder = "Wähle eine Kategorie", label_visibility = "collapsed")
     st.session_state.category = cat
 
     submit = False
     if cat is not None:
         y = list(util.stu_qa.find({"category": cat}, sort=[("rang", pymongo.ASCENDING)]))        
         with st.popover(f'Neues QA-Paar anlegen'):
-            studiengang_list = st.multiselect("Studiengänge (alle, falls keiner angegeben ist)", [x["_id"] for x in util.studiengang.find({}, sort = [("rang", pymongo.ASCENDING)])], [], format_func = (lambda a: tools.repr(util.studiengang, a, False)), placeholder = "Bitte auswählen", key = "new_stu_list")
+            studiengang_list = st.multiselect("Studiengänge (alle, falls keiner angegeben ist)", [x['_id'] for x in util.studiengang.find({}, sort = [("rang", pymongo.ASCENDING)])], [], format_func = (lambda a: tools.repr(util.studiengang, a, False)), placeholder = "Bitte auswählen", key = "new_stu_list")
             q_de = st.text_input("Frage (de)", "", key = "new_q_de")
             q_en = st.text_input("Frage (en)", "", key = "new_q_en")
             a_de = st.text_input("Antwort (de)", "", key = "new_a_de")
@@ -53,16 +53,16 @@ if st.session_state.logged_in:
         for x in y:
             co1, co2, co3, co4 = st.columns([1,1,20,4]) 
             with co1: 
-                st.button('↓', key=f'down-{x["_id"]}', on_click = tools.move_down, args = (collection, x, {"category" : st.session_state.category},))
+                st.button('↓', key=f"down-{x['_id']}", on_click = tools.move_down, args = (collection, x, {"category" : st.session_state.category},))
             with co2:
-                st.button('↑', key=f'up-{x["_id"]}', on_click = tools.move_up, args = (collection, x, {"category" : st.session_state.category},))
+                st.button('↑', key=f"up-{x['_id']}", on_click = tools.move_up, args = (collection, x, {"category" : st.session_state.category},))
             with co3: 
-                with st.expander(f"{x['q_de']}", expanded = (True if x["_id"] == st.session_state.expanded else False)):
+                with st.expander(f"{x['q_de']}", expanded = (True if x['_id'] == st.session_state.expanded else False)):
                     st.write(f"qa_{str(x['_id'])}")
-                    index = [cat["_id"] for cat in cats].index(x["category"])
+                    index = [cat['_id'] for cat in cats].index(x["category"])
                     
-                    cat_loc = st.selectbox(label="Kategorie", options = [z["_id"] for z in cats], index = ([z["_id"] for z in cats]).index(x["category"]), format_func = lambda id: tools.repr(util.stu_category, id, False), placeholder = "Wähle eine Kategorie", label_visibility = "collapsed", key = f"stu_cat_{x["_id"]}")
-                    studiengang_list = st.multiselect("Studiengänge (alle, falls keiner angegeben ist)", [x["_id"] for x in util.studiengang.find({}, sort = [("rang", pymongo.ASCENDING)])], x["studiengang"], format_func = (lambda a: tools.repr(util.studiengang, a, False)), placeholder = "Bitte auswählen", key = f"stu_list_{x["_id"]}")
+                    cat_loc = st.selectbox(label="Kategorie", options = [z['_id'] for z in cats], index = ([z['_id'] for z in cats]).index(x["category"]), format_func = lambda id: tools.repr(util.stu_category, id, False), placeholder = "Wähle eine Kategorie", label_visibility = "collapsed", key = f"stu_cat_{x['_id']}")
+                    studiengang_list = st.multiselect("Studiengänge (alle, falls keiner angegeben ist)", [x['_id'] for x in util.studiengang.find({}, sort = [("rang", pymongo.ASCENDING)])], x["studiengang"], format_func = (lambda a: tools.repr(util.studiengang, a, False)), placeholder = "Bitte auswählen", key = f"stu_list_{x['_id']}")
                     q_de = st.text_input('Frage (de)', x["q_de"], placeholder="Frage eingeben", key = f"q_de_{x['_id']}")
                     q_en = st.text_input('Frage (en)', x["q_en"], key = f"q_en_{x['_id']}")
                     a_de = st.text_area('Antwort (de)', x["a_de"], placeholder="Antwort eingeben", key = f"a_de_{x['_id']}")
@@ -76,7 +76,7 @@ if st.session_state.logged_in:
                 with st.popover('Löschen', use_container_width=True):
                     colu1, colu2, colu3 = st.columns([1,1,1])
                     with colu1:
-                        submit = st.button(label = "Wirklich löschen!", type = 'primary', key = f"delete-{x['_id']}", on_click = tools.delete_item_update_dependent_items, args = (collection, x["_id"],))
+                        submit = st.button(label = "Wirklich löschen!", type = 'primary', key = f"delete-{x['_id']}", on_click = tools.delete_item_update_dependent_items, args = (collection, x['_id'],))
                     with colu3: 
                         st.button(label="Abbrechen", on_click = st.success, args=("Nicht gelöscht!",), key = f"not-deleted-{x['_id']}")
 
